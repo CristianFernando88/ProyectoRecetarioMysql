db8a54414eaf6819cb66a82b026a11e0fc7550c1
CristianFernando <gordyfernando@gmail.com>
2023-07-14 22:34:39 -0300

    creacion de credenciales

---
+++ credenciales.py
@@ -0,0 +1,7 @@
+# modificar credenciales solo aqui
+credenciales = {'user': 'root',
+                'password': '1088',
+                'host': '127.0.0.1',
+                'database': 'todo_app'}
+# icono
+icono = "./img/todo.ico"
